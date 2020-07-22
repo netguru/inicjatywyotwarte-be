148880bfcaf334870b2a478161c1c42acd6af64e
Michał Wazgird <michal.wazgird@netguru.com>
2020-07-22 11:48:54 +0200

    remove unused variable from test

--- spec/api/v1/resources/vote_spec.rb
+++ spec/api/v1/resources/vote_spec.rb
@@ -33,67 +33,67 @@ RSpec.describe V1::Resources::Vote, type: :request do
     include_examples 'returns 400'
   end
 
-  context 'when value 1' do
-    before do
-      allow(::Resources::Upvote).to receive(:new).and_return(upvote_service_double)
-      allow(upvote_service_double).to receive(:call).and_return(upvote_service_double)
-    end
-
-    let(:upvote_service_double) { double(:upvote_service, call: nil, success?: success) }
-    let(:vote_value) { 1 }
+  context 'with proper resource' do
     let(:resource) { FactoryBot.create(:resource, :approved) }
     let(:resource_id) { resource.id }
-    let(:success) { true }
 
-    it 'calls proper service' do
-      expect(::Resources::Upvote).to receive(:new).with(resource, ip_address) { upvote_service_double }
-      subject
-    end
+    context 'when value 1' do
+      before do
+        allow(::Resources::Upvote).to receive(:new).and_return(upvote_service_double)
+        allow(upvote_service_double).to receive(:call).and_return(upvote_service_double)
+      end
 
-    context 'when service call is a success' do
-      it 'serializes resource with proper serializer' do
+      let(:upvote_service_double) { double(:upvote_service, call: nil, success?: success) }
+      let(:vote_value) { 1 }
+      let(:success) { true }
+
+      it 'calls proper service' do
+        expect(::Resources::Upvote).to receive(:new).with(resource, ip_address) { upvote_service_double }
         subject
-        expect(response_json).to be_serialization_of(resource, with: ResourceSerializer)
+      end
+
+      context 'when service call is a success' do
+        it 'serializes resource with proper serializer' do
+          subject
+          expect(response_json).to be_serialization_of(resource, with: ResourceSerializer)
+        end
+      end
+
+      context 'when service call is a failure' do
+        let(:success) { false }
+
+        include_examples 'returns 403'
       end
     end
 
-    context 'when service call is a failure' do
-      let(:success) { false }
+    context 'when value 0' do
+      before do
+        allow(::Resources::RevokeUpvote).to receive(:new).and_return(revoke_upvote_service_double)
+        allow(revoke_upvote_service_double).to receive(:call).and_return(revoke_upvote_service_double)
+      end
 
-      include_examples 'returns 403'
-    end
-  end
+      let(:revoke_upvote_service_double) { double(:revoke_upvote_service, call: nil, success?: success) }
+      let(:vote_value) { 0 }
+      let(:success) { true }
 
-  context 'when value 0' do
-    before do
-      allow(::Resources::RevokeUpvote).to receive(:new).and_return(revoke_upvote_service_double)
-      allow(revoke_upvote_service_double).to receive(:call).and_return(revoke_upvote_service_double)
-    end
-
-    let(:revoke_upvote_service_double) { double(:revoke_upvote_service, call: nil, success?: success) }
-
-    let(:ip_address) { IPAddr.new(FFaker::Internet.ip_v4_address) }
-    let(:vote_value) { 0 }
-    let(:resource) { FactoryBot.create(:resource, :approved) }
-    let(:resource_id) { resource.id }
-    let(:success) { true }
-
-    it 'calls proper service' do
-      expect(::Resources::RevokeUpvote).to receive(:new).with(resource, ip_address) { revoke_upvote_service_double }
-      subject
-    end
-
-    context 'when service call is a success' do
-      it 'serializes resource with proper serializer' do
+      it 'calls proper service' do
+        expect(::Resources::RevokeUpvote).to receive(:new).with(resource, ip_address) { revoke_upvote_service_double }
         subject
-        expect(response_json).to be_serialization_of(resource, with: ResourceSerializer)
+      end
+
+      context 'when service call is a success' do
+        it 'serializes resource with proper serializer' do
+          subject
+          expect(response_json).to be_serialization_of(resource, with: ResourceSerializer)
+        end
+      end
+
+      context 'when service call is a failure' do
+        let(:success) { false }
+
+        include_examples 'returns 403'
       end
     end
-
-    context 'when service call is a failure' do
-      let(:success) { false }
-
-      include_examples 'returns 403'
-    end
   end
+
 end
